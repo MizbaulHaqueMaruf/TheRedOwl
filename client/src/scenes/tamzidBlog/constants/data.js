588e79c48e5f5565a79e9a3f850204c48a06ee3e
Tamzid86 <syedtamzid@iut-dhaka.edu>
2023-03-15 01:01:19 +0600

    constants

---
+++ client/src/scenes/tamzidBlog/constants/data.js
@@ -0,0 +1,9 @@
+
+
+export const categories=[
+    {id: 1, type:'Coding Practices'},
+    {id:2, type:'Machine Learning'},
+    {id:3, type:'Blockchain Technology'},
+    {id:4, type:'Cyber Security'},
+    {id:5, type:'Materials Science'}
+]
