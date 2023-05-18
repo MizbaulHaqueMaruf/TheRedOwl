9251db30805cbba13a6d114391ce57d4cf57792c
Tamzid86 <syedtamzid@iut-dhaka.edu>
2023-05-19 02:44:18 +0600

    (css-part-1)

--- client/src/scenes/tamzidBlog/Blogs.jsx
+++ client/src/scenes/tamzidBlog/Blogs.jsx
@@ -2,8 +2,76 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setBlogs } from "state";
 import Blog from "./Blog";
+import { useState } from 'react';
+import Navbar from "scenes/navbar/blognavbar";
+import IUTImage from './img/iut.jpg';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  cardContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    position: 'sticky',
+    top: '70px',
+    right: '30px',
+    width: 'fit-content',
+  },
+  card: {
+    display: 'flex',
+    width: '300px',
+    marginRight: '20px',
+  },
+  image: {
+    width: '300px',
+    height: '130px',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+}));
 
 const Blogs = ({ userId, isProfile = false }) => {
+
+  const classes = useStyles();
+
+  //that line
+  const [showLine, setShowLine] = useState(true);
+  const sentence = "For IUTians, To IUTians, By IUTians";
+  const words = sentence.split(" ");
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 1000); // 1000 milliseconds = 1 second
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [currentWordIndex, words.length]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowLine(false);
+      setTimeout(() => {
+        setShowLine(true);
+        setCurrentWordIndex(0);
+      }, 500); // 500 milliseconds
+    }, words.length * 1000); // Total duration of word-by-word display
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [words.length]);
+
+
+
   const dispatch = useDispatch();
   const blogs = useSelector((state)=> state.blogs);
   const token = useSelector((state) => state.token);
@@ -35,12 +103,37 @@ const Blogs = ({ userId, isProfile = false }) => {
     } else {
       getBlogs();
     }
-  }, []); 
+  }, []);
 
   
   return (
     <div>
-      {blogs.map(
+      <Navbar />
+      <div >
+        <div style={{ height: '2px', textAlign: 'left', position:'sticky', top:'70px', marginLeft:'30px' }}>
+        
+          {showLine && (
+            <div style={{ textAlign: 'left' }}>
+              {words.map((word, index) => (
+                <div
+                  key={index}
+                  style={{
+                    opacity: currentWordIndex >= index ? 1 : 0,
+                    fontSize: '50px', // Adjust the font size as needed
+                    color:'#5C0404',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  {word}
+                  {index !== words.length - 1 && " "}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        
+        <div style={{ padding: '0 350px',}}>
+        {blogs.map(
         ({
           _id,
           userId,
@@ -50,7 +143,6 @@ const Blogs = ({ userId, isProfile = false }) => {
           role,
           picturePath,
           userPicturePath,
-          register,
         }) => (
           <Blog
           key={_id}
@@ -61,17 +153,26 @@ const Blogs = ({ userId, isProfile = false }) => {
           role={role}
           picturePath={picturePath}
           userPicturePath={userPicturePath}
-          register={register}
-          style={{
-            maxWidth: '500px',
-            border: '1px solid #ddd',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-            borderRadius: '5px',
-            overflow: 'hidden'
-          }}
         />
       )
     )}
+    
+        </div>
+
+    </div>
+
+    <div class="card" style={{boxShadow:'0 4px 8px 4px rgba(0,0,0,0.2', height:'450px', maxWidth: '303px'}}>
+      <img src={IUTImage} style={{width:'100%'}}/>
+      <div class="container" style={{padding: '2px 16px'}}>
+        <div style={{ fontWeight: 'bold', fontSize: '20px', fontFamily: 'cursive', color: '#5C0404', textAlign: 'center', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+          <h4>
+            Embark on a journey of <span style={{ color: '#5C0404' }}>self-expression</span>, where every word paints a vivid canvas of <span style={{ color: '#5C0404' }}>thoughts</span> and <span style={{ color: '#5C0404' }}>ideas</span>.
+          </h4>
+        </div>
+
+      </div>
+    </div>
+    
 </div>
   );
 };
