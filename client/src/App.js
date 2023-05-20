28eb217f42fededcc4c5c0bdc6591a60a49a2fb9
Mizbaul Haque Maruf <mizbaulhaque@iut-dhaka.edu>
2023-05-20 23:17:16 +0600

    bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
-import ProfilePage from "scenes/sadafprofile/Profile.js";
+import ProfilePage from "scenes/sadafProfile/Profile.js";
 import BlogPage from "scenes/tamzidBlog/MyBlog.jsx";
 import BlogDetails from "scenes/tamzidBlog/BlogDetails.jsx";
 import AllBlogs from "scenes/tamzidBlog/Blogs.jsx";
